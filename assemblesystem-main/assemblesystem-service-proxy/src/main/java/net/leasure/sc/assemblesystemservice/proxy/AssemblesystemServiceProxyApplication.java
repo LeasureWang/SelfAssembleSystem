e081cdc8a26aa13397d8006bea77d1ea356d0036
Leasure_WANG <leasure_wang@outlook.com>
2021-03-14 15:13:18 +0800

    none just space

--- assemblesystem-main/assemblesystem-service-proxy/src/main/java/net/leasure/sc/assemblesystemservice/proxy/AssemblesystemServiceProxyApplication.java
+++ assemblesystem-main/assemblesystem-service-proxy/src/main/java/net/leasure/sc/assemblesystemservice/proxy/AssemblesystemServiceProxyApplication.java
@@ -2,8 +2,10 @@ package net.leasure.sc.assemblesystemservice.proxy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"net.leasure.sc"})
 public class AssemblesystemServiceProxyApplication {
 
     public static void main(String[] args) {
