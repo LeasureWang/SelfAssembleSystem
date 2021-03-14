db5a5aca9aafc1ce16f227979271ee424cf28453
Leasure_WANG <leasure_wang@outlook.com>
2021-03-14 09:14:27 +0800

    none just space

--- assemblesystem-main/assemblesystem-service-consumer/src/main/java/net/leasure/sc/assemblesystemservice/consumer/AssemblesystemServiceConsumerApplication.java
+++ assemblesystem-main/assemblesystem-service-consumer/src/main/java/net/leasure/sc/assemblesystemservice/consumer/AssemblesystemServiceConsumerApplication.java
@@ -11,5 +11,4 @@ public class AssemblesystemServiceConsumerApplication {
     public static void main(String[] args) {
         SpringApplication.run(AssemblesystemServiceConsumerApplication.class, args);
     }
-
 }
